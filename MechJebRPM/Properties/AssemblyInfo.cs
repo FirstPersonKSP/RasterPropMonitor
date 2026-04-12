20fcb3b44723372f09a773ae3e2dfb67c2e59d20
JonnyOThan <jonnyothan@gmail.com>
2026-04-12 09:30:41 -0400

    use ModReference for MJ and fix up assembly dependency for ProjectReference

--- MechJebRPM/Properties/AssemblyInfo.cs
+++ MechJebRPM/Properties/AssemblyInfo.cs
@@ -5,6 +5,3 @@ using System.Runtime.CompilerServices;
 // Change them to the values specific to your project.
 [assembly: AssemblyDescription("RasterPropMonitor / MechJeb2 interface plugin for Kerbal Space Program")]
 
-// NOTE: ideally this would be generated from KSPBuildTools, but it's not working properly yet.
-// eventually, get that working in the csproj file and then remove this line.
-[assembly: KSPAssemblyDependency("RasterPropMonitor", 1, 0)]
